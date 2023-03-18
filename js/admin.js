52836cf176416a843b57998e58f2a7ce84fe8516
Andrew Tam <andrew360tam@gmail.com>
2023-03-18 11:57:07 -0400

    my changes

---
+++ js/admin.js
@@ -0,0 +1,41 @@
+//book fields from admin.html
+let bookFields = []
+for (let i = 0; i < 5; ++i) {
+    let cur_selector = "[name='book" + String(i + 1) + "']";
+    bookFields.push(document.querySelector(cur_selector));
+}
+//submit button from admin.html
+let bookSubmit = document.querySelector("#submit-button")
+bookSubmit.addEventListener("click", getOrder);
+
+function getOrder() {
+    //push all of the books to a list
+    let bookList = [];
+    for (let i = 0; i < 5; ++i) {
+        bookList.push(bookFields[i].value);
+    }
+    //check for duplicates
+    for (let i = 0; i < 5; ++i) {
+        for (let j = 0; j < i; ++j) {
+            if (bookList[i] === bookList[j]) {
+                alert("The book \"" + bookList[i] + "\" has been selected twice. Please make another selection.");
+                return;
+            }
+        }
+    }
+    //store the order in local storage
+    for (let i = 0; i < 5; ++i) {
+        localStorage.setItem("book" + String(i + 1), bookList[i]);       
+    }
+}
+
+function getBook(bookNum) {
+    //returns the book numbered bookNum in the list
+    //precondition: 1 <= bookNum <= 5
+    const defaultOrder = ["The Maid", "Sea of Tranquility", "Hidden Pictures", "House of Sky and Breath", "The Final Gambit"];
+    let bookTitle = localStorage.getItem("book" + String(bookNum));
+    if (bookTitle !== null) {
+        return bookTitle;
+    }
+    return defaultOrder[bookNum];
+}
--- js/login.js
+++ js/login.js
@@ -1,36 +1,32 @@
 //our list of users
-//note that "admin" is user 0
 let users = ["admin", "Andrew", "Zuhair", "Murphy"];
 //username field from login.html
 let usernameField = document.querySelector("#login-username");
-//username button from login.html
+//submit button from login.html
 let usernameSubmit = document.querySelector("#login-button");
 usernameSubmit.addEventListener("click", logUser);
-
-let tmp = document.querySelector("#tmp");
-tmp.innerHTML = String(getUser());
 
 function logUser() {
     //get the username from the username field
     let username = usernameField.value;
-    //find out which user corresponds to the given username
-    let user = -1;
+    //find out which position corresponds to the given username
+    let pos = -1;
     for (let i = 0; i < users.length; ++i) {
         if (username === users[i]) {
-            user = i;
+            pos = i;
         }
     }
-    //if the user is not in our list of users,
+    //if the username is not in our list of users,
     //alert the user and terminate the function
-    if (user === -1) {
-        alert("Please enter a valid username");
-        localStorage.clear();
+    if (pos === -1) {
+        alert("Please enter a valid username.");
+        localStorage.removeItem("current-user");
         return;
     }
-    //store the user in local storage 
-    localStorage.setItem("current-user", String(user));
+    //store the username in local storage 
+    localStorage.setItem("current-user", username);
     //redirect the page
-    if (user === 0) {
+    if (username === "admin") {
         location.replace("admin.html");
     } else {
         location.replace("book-info.html");
@@ -39,10 +35,10 @@ function logUser() {
 
 function getUser() {
     //get who the current user is from local storage
-    //the user is set to -1 if the user is not logged in
-    let strUser = localStorage.getItem("current-user");
-    if (strUser === null) {
-        return -1;
+    //the user is set to "none" if the no one is logged in
+    let username = localStorage.getItem("current-user");
+    if (username === null) {
+        return "none";
     }
-    return parseInt(strUser);
+    return username;
 }
