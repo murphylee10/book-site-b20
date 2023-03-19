10e1dad6db1a35929bf3fe6cd35dc08d50079271
murphylee10 <murphylee2004@gmail.com>
2023-03-19 12:28:51 -0400

    js

---
+++ js/book-info.js
@@ -0,0 +1,25 @@
+let titleField = document.querySelector("#review-book-choice");
+let textField = document.querySelector("#review-input");
+let reviewSubmit = document.querySelector("#review-submit");
+
+reviewSubmit.addEventListener("click", storeReview);
+
+function storeReview() {
+    let title = titleField.value;
+    let reviewText = textField.value;
+    textField.value = "";
+    if (getUser() === "none") {
+        alert("Please log in first.");
+        return;
+    }
+    let reviewNum = localStorage.getItem("numReviews");
+    if (reviewNum === null) {
+        reviewNum = 1;
+    } else {
+        reviewNum = Number(reviewNum) + 1;
+    }
+    localStorage.setItem("numReviews", reviewNum);
+    localStorage.setItem("reviewUser" + String(reviewNum), getUser());
+    localStorage.setItem("reviewTitle" + String(reviewNum), title);
+    localStorage.setItem("reviewText" + String(reviewNum), reviewText);
+}
